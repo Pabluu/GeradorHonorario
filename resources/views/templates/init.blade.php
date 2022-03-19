8985acf360fec8f1a643e833c4fb3b9e0e11013a
Pabluu <pabloph0077@gmail.com>
2022-03-19 13:51:50 -0300

    criaçao do template

---
+++ resources/views/templates/init.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>@yield("titulo")</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+    <link rel="stylesheet" href="{{url(mix('te/css/style.css'))}}">
+</head>
+
+<body>
+    <div class="container">
+        @yield("titulo")
+        @yield("formulario")
+        @yield("mensagem")
+    </div>
+</body>
+
+</html>
